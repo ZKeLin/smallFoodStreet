689e444bff91db6e6d28a5031b0bc548a8768f1c
kelin <2312677078@qq.com>
2017-06-24 14:29:19 +0800

    Update others.js

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,9 +1,9 @@
 /* GET 'about' page */
 module.exports.about = function(req,res){
 	res.render('about',{
-		title: 'about',
-		content: ['Loc8r was created to help people find places to sit down and get abit of work done.',
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sedlorem ac nisi dignissim accumsan.'
+		title: '关于我们',
+		content: ['小食街是一个专门用来吃着小食寻找wifi，美女，帅哥的地方',
+			'在这里，有你想吃的，有你想看的^_^^_^^_^'
 		]
 	});
 };
